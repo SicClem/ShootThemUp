c0d20e77535bdc5bbc974efda5ce12e1f174d1ac
SICHE <clement.siche@gmail.com>
2019-10-30 17:26:11 +0100

    mouvment zombie

--- src/app/Component/zombie/zombie.component.ts
+++ src/app/Component/zombie/zombie.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zombie.component.scss']
 })
 export class ZombieComponent implements OnInit {
+  isArrived : boolean = false;
+  position : number = 0
 
   constructor() { }
 
   ngOnInit() {
+    this.interval()
+  }
+
+ interval() {
+   setInterval(()=> {
+    this.stopZombie();
+    if (this.isArrived === false){
+      this.movePosition();
+      console.log(this.position);
+      }
+    }, 20);
+  }
+
+  stopZombie() {
+    if (this.position === window.innerWidth - 200) {
+      this.isArrived = true;
+    }
+  }
+
+  movePosition () {
+    this.position += 10;
   }
 
 }
