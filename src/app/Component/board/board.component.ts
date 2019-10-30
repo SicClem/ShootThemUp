22eeb543e1981a336458c6161d33df9f7dbc7786
SICHE <clement.siche@gmail.com>
2019-10-31 00:57:37 +0100

    deportage service

--- src/app/Component/board/board.component.ts
+++ src/app/Component/board/board.component.ts
@@ -9,29 +9,13 @@ import { GameService } from 'src/app/Shared/game.service';
 })
 export class BoardComponent implements OnInit {
 
-  public singleZombie: Zombie[] = [];
-  public zombies: Zombie [];
+  singleZombie: Zombie[] = [];
+  zombies: Zombie [];
 
-  constructor(private zombService: GameService ) {}
+  constructor(private zombService: GameService) {}
 
-  public ngOnInit(): void {
-    this.zombService.getZombies().subscribe(
-        (response: any) => {
-            this.zombies = response.monsters;
-        }
-    );
-
-    setInterval(() => {
-      this.addSingleZombie();
-    }, 1000);
-  }
-
-  /**
-   * Add a random zombie from the array zombies.
-   */
-  addSingleZombie() {
-    const randomIndex = Math.floor(Math.random() * this.zombies.length);
-    const randomZombie = this.zombies[randomIndex]
-    this.singleZombie.push(randomZombie);
+  ngOnInit(): void {
+    this.zombies = this.zombService.zombies;
+    this.singleZombie = this.zombService.singleZombie;
   }
 }
