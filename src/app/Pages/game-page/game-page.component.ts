e8045f99e1711a94b2f8359a7ab337b90fa878b2
HugoBoxFr <hugo.prea@sfr.fr>
2019-10-30 21:13:06 +0100

    eclair son

--- src/app/Pages/game-page/game-page.component.ts
+++ src/app/Pages/game-page/game-page.component.ts
@@ -6,17 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-page.component.scss']
 })
 export class GamePageComponent implements OnInit {
-  storm: string;
+  bckgrd_default: string = "url('/assets/bckgrd-1.svg')";
+  bckgrd_flash : string = "url('/assets/bckgrd-2.png')";
+  bckgrd: string = "url('/assets/bckgrd-1.svg')";
+  // sound: string = "";
+  // soundStorm = new Audio("url('/assets/tonnere.mp3')");
+  audio = new Audio ('/assets/tonnerre.wav');
+
 
   constructor() { 
 
-  }
-
-  flash(){
     setInterval(() => {
-      this.storm === "none"}, 30000
-    );
-  }
+      this.bckgrd = this.bckgrd_flash;
+      setTimeout(() => {
+        this.bckgrd = this.bckgrd_default;
+        setTimeout(() => {
+          this.bckgrd = this.bckgrd_flash;
+          setTimeout(() => {
+            this.bckgrd = this.bckgrd_default;
+          }, 100);
+          this.bckgrd = this.bckgrd_flash;
+        }, 100);
+        this.bckgrd = this.bckgrd_default;
+      }, 200);
+      this.audio.play();
+      this.bckgrd = this.bckgrd_flash;
+    }, 30000);
+  };
 
   ngOnInit() {};
+
 }
